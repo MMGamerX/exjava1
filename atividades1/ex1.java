2fbbe4085593e507dcbf4d1b30432765dbfd4265
MM <mmgamerx2010@gmail.com>
2017-06-01 14:47:47 -0400

    Resolução do ex1
    Não sei se estou fazendo isso certo, (estou falando do github)

--- atividades1/ex1.java
+++ atividades1/ex1.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
 public class ex1 {
-  public static void main(String[] args){
-    // Ex1...
+  public static void main(String args[]){
+    int[] numeros;
+    numeros = new int[10];
+    Scanner sc;
+    sc = new Scanner(System.in);
+    for (int c=0;c<10;c++) {
+      System.out.println("Digite o "+(c+1)+"° número: ");
+      numeros[c]=sc.nextInt();
+    }
+    System.out.println("Os seguintes números são pares:");
+    for (int c=0;c<10;c++){
+      if (numeros[c]%2==0){
+        System.out.print(numeros[c]+" ");
+      }
+    }
   }
 }
